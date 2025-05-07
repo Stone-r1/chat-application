d449d56168f8551045067b38d0137bf27bb00260
stoney <sarkisyanmihail11@gmail.com>
2025-05-08 03:50:03 +0400

    polishing server side

--- src/server.c
+++ src/server.c
@@ -3,40 +3,58 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
 
 #define MYPORT 7270
 #define MAXUSERS 5
-#define BUFFERSIZE 1025
+#define BUFFERSIZE 1024
 
 /* TODO
- * add multpiple client handling (concurency)
  * log client IP/port
  * normal shut down handling
  * handle partial reads/writes
- * add signal handler to close socket
 */
 
-int main() {
-
-    // add separate function for readability
-    char buffer[BUFFERSIZE] = {0};
-    int sock = socket(AF_INET6, SOCK_STREAM, 0), newSock; // creating socket 
+int createSocket() {
+    int sock = socket(AF_INET6, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("socket");
         return 1;
-    }    
+    }
+    return sock;
+}
+
+void setupServerDetails(struct sockaddr_in6* addr) {
+    memset(addr, 0, sizeof(struct sockaddr_in6));
+    addr -> sin6_family = AF_INET6; // specifies family
+    addr -> sin6_addr = in6addr_any; // bind to any local address
+    addr -> sin6_port = htons(MYPORT);    
+}
+
+void handleClient(int clientSocket) {
+    char buffer[BUFFERSIZE + 1] = {0};
+    int message;
+    while ((message = read(clientSocket, buffer, BUFFERSIZE)) > 0) {
+        buffer[message] = '\0';
+        printf("Client: %s", buffer);
+        const char* replyText = "Server has received your message\n";
+        write(clientSocket, replyText, strlen(replyText));
+    }
+}
+
+int main() {
+    int sock = createSocket(), newSock;
+    signal(SIGCHLD, SIG_IGN); // automatically clean up 'zombie' processes
     
     int opt = 1;
     setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     struct sockaddr_in6 clientAddr, serverAddr; // ipv6 socket address
     socklen_t clientAddrLen = sizeof(clientAddr), serverAddrLen = sizeof(serverAddr);
-    
-    memset(&serverAddr, 0, serverAddrLen);
-    serverAddr.sin6_family = AF_INET6; // specifies family
-    serverAddr.sin6_addr = in6addr_any; // bind to any local address
-    serverAddr.sin6_port = htons(MYPORT);
+    setupServerDetails(&serverAddr);
+
     
     // binds local name to the socket
     if (bind(sock, (struct sockaddr*) &serverAddr, serverAddrLen) < 0) {
@@ -44,26 +62,30 @@ int main() {
         return 1;
     }    
 
-    // up to 5 users TODO: add concurency as of right now it receives one user and then dies.
     if (listen(sock, MAXUSERS) < 0) {
         perror("listen");
         return 1;
+    } else {
+        while (1) {
+            int clientSocket = accept(sock, (struct sockaddr*) &clientAddr, &clientAddrLen);
+            if (clientSocket < 0) {
+                perror("accept");
+                return 1;
+            }
+
+            pid_t pid = fork();
+            if (pid < 0) {
+                perror("fork");
+                close(clientSocket);
+                return 1;
+            } else if (pid == 0) {
+                close(sock); // close the listening socket in the child
+                handleClient(clientSocket);
+                close(clientSocket);
+                exit(EXIT_SUCCESS);
+            } else {
+                close(clientSocket);
+            }
+        }
     }
-
-    // replace with a loop and threading/forking for multiple clients
-    if ((newSock = accept(sock, (struct sockaddr*) &clientAddr, &clientAddrLen)) < 0) {
-        perror("accept");
-        return 1;
-    }
-
-    // move to a separate function
-    int message;
-    while ((message = read(newSock, buffer, BUFFERSIZE - 1)) > 0) {
-        buffer[message] = '\0';
-        printf("Client: %s", buffer);
-        write(newSock, "Server has received your message!\n", 34);
-    };
-
-    close(newSock);
-    close(sock);
 }
