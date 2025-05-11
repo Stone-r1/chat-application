2b851fce33467868208d86bc3103c1f4f80c6015
stoney <sarkisyanmihail11@gmail.com>
2025-05-11 21:33:44 +0400

    working version

--- src/server.c
+++ src/server.c
@@ -8,14 +8,11 @@
 #include <arpa/inet.h>
 #include <signal.h>
 #include <sys/select.h>
+#include <pthread.h>
 
 #define MYPORT 7270
 #define MAXUSERS 5
 #define BUFFERSIZE 1024
-
-/* TODO
- * handle partial reads/writes
-*/
 
 volatile sig_atomic_t running = 1;
 
@@ -23,11 +20,43 @@ void handleShutdown(int signal) {
     running = 0;
 }
 
+// = neccessary tools =
+typedef struct {
+    int sockfd;
+} client_t;
+
 struct SocketWrapper {
     int sock;
     struct sockaddr_in6 addr;
     int family;
 };
+
+static pthread_mutex_t clientMutex = PTHREAD_MUTEX_INITIALIZER; // unlocked mutex
+static client_t *clientList[MAXUSERS]; // mutex used to ensure that only on thread can modify the array at a time.
+// ====================
+
+void addClient(client_t* client) {
+    pthread_mutex_lock(&clientMutex);
+    for (int i = 0; i < MAXUSERS; i++) {
+        if (!clientList[i]) {
+            clientList[i] = client;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clientMutex);
+}
+
+void removeClient(int fd) {
+    pthread_mutex_lock(&clientMutex);
+    for (int i = 0; i < MAXUSERS; i++) {
+        if (clientList[i] && clientList[i] -> sockfd == fd) {
+            free(clientList[i]);
+            clientList[i] = NULL;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clientMutex);
+}
 
 int createSocket(int family) {
     int sock = socket(family, SOCK_STREAM, 0);
@@ -77,15 +106,26 @@ ssize_t sendAll(int socket, const char* buffer, size_t length) {
     return totalSent;
 }
 
-void handleClient(int clientSocket) {
+void* handleClient(void* arg) {
+    client_t* client = (client_t*)arg;
+    int clientSocket = client -> sockfd;
+    // int clientSocket = *(int*)arg; // casting void* to int* and dereferencing
+    // free(arg);
+
     char buffer[BUFFERSIZE + 1] = {0};
     int message;
     while ((message = read(clientSocket, buffer, BUFFERSIZE)) > 0) {
         buffer[message] = '\0';
-        printf("Client: \"%s\"\n", buffer);
+        printf("Client [%d]: \"%s\"\n",clientSocket, buffer);
         const char* replyText = "Server has received your message\n";
         sendAll(clientSocket, replyText, strlen(replyText));
     }
+
+    // cleanup
+    close(clientSocket);
+    removeClient(clientSocket);
+    printf("Client[%d] disconnected.\n", clientSocket);
+    return NULL;
 }
 
 // Prepares the set of sockets to be monitored
@@ -95,7 +135,7 @@ void selectSockets(fd_set* readfds, SocketWrapper* ipv4Socket, SocketWrapper* ip
     FD_SET(ipv6Socket -> sock, readfds); // monitor for events
 }
 
-int acceptConnection(SocketWrapper* socketWrapper, fd_set* readfds, int* clientSockets, int* clientAmount) {
+void acceptConnection(SocketWrapper* socketWrapper, fd_set* readfds) {
     struct sockaddr_in6 clientAddr;
     socklen_t clientAddrLen = sizeof(clientAddr);
 
@@ -103,33 +143,30 @@ int acceptConnection(SocketWrapper* socketWrapper, fd_set* readfds, int* clientS
         int clientSocket = accept(socketWrapper -> sock, (struct sockaddr*) &clientAddr, &clientAddrLen);
         if (clientSocket < 0) {
             perror("accept");
-            return 1;
+            return;
         }
 
         char clientIP[INET6_ADDRSTRLEN];
         inet_ntop(socketWrapper -> family, &clientAddr.sin6_addr, clientIP, sizeof(clientIP));
-        // ntohs -> converts the port number from network byte order to host byte order 
+        // ntohs -> converts the port number from network byte order to host byte order
+        printf("New client: %d\n", clientSocket);
         printf("IP address: %s\n", clientIP);
         printf("Port      : %d\n", ntohs(clientAddr.sin6_port));
 
-        clientSockets[*clientAmount] = clientSocket;
-        (*clientAmount)++;
+        client_t* client = (client_t*)malloc(sizeof *client);
+        client -> sockfd = clientSocket;
+        addClient(client);
 
-        printf("New Client Connected!\n");
-        //handleClient(clientSocket);
-        //close(clientSocket);
-    }
-
-    return 0;
-}
-
-void removeClient(int* clientSockets, int* clientAmount, int clientSocket) {
-    for (int i = 0; i < *clientAmount; i++) {
-        if (clientSockets[i] == clientSocket) {
+        pthread_t threadID;
+        if (pthread_create(&threadID, NULL, handleClient, client) != 0) {
+            perror("pthread_create");
+            removeClient(clientSocket);
             close(clientSocket);
-            clientSockets[i] = clientSockets[--(*clientAmount)];
-            break;
+            free(client);
+            return;
         }
+
+        pthread_detach(threadID);
     }
 }
 
@@ -143,8 +180,8 @@ int main(int args, char* argv[]) {
         return 1;
     }
     
-    int opt = 1;
-    if (setsockopt(ipv6Socket.sock, IPPROTO_IPV6, IPV6_V6ONLY, &opt, sizeof(opt)) < 0) {
+    int optionValue = 1;
+    if (setsockopt(ipv6Socket.sock, IPPROTO_IPV6, IPV6_V6ONLY, &optionValue, sizeof(optionValue)) < 0) {
         perror("setsockopt IPV6_V6ONLY");
         return 1;
     }
@@ -204,14 +241,12 @@ int main(int args, char* argv[]) {
             return 1;
         }
 
-        acceptConnection(&ipv6Socket, &readfds, clientSockets, &clientAmount);
-        acceptConnection(&ipv4Socket, &readfds, clientSockets, &clientAmount);
-
-        for (int i = 0; i < clientAmount; i++) {
-            if (FD_ISSET(clientSockets[i], &readfds)) {
-                handleClient(clientSockets[i]);
-                removeClient(clientSockets, &clientAmount, clientSockets[i]);
-            }
-        }
+        acceptConnection(&ipv6Socket, &readfds);
+        acceptConnection(&ipv4Socket, &readfds);
     }
+
+    close(ipv6Socket.sock);
+    close(ipv4Socket.sock);
+    printf("Server Shutdown.\n");
+    return 0;
 }
